4305495e13bd2a9aae812d69a4e4e37c7d019253
minhduc1222 <frostgamer150@gmail.com>
2025-04-15 13:34:27 +0700

    temporary completed: fix place order, add enquiry

--- layout-mobile.html.php
+++ layout-mobile.html.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- a -->
     <meta charset="UTF-8">
     <title>CheapDeals</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
